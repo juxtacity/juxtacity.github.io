cb3c9571fd30d9c9381cf264413b51f312124f8f
juxtacity <jbenlindquist@gmail.com>
2025-05-28 18:45:56 -0600

    added code for blog posts but not comments yet
    also wrote two posts

---
+++ BlogPosts.js
@@ -0,0 +1,19 @@
+blogPosts = [
+
+     {
+        title: "Dummy Post",
+        date: "May 28th, 2025",
+        content: [
+            "I'm only really writing this to test the functionality of my code.",
+            "I might keep it in though, for historical purposes, (and for the kicks and the giggles)."
+        ]
+    },
+    {
+        title: "Website Building Fur Days",
+        date: "May 28th, 2025",
+        content: [
+            "I've been building this website for a few days now, based off of the code I had from one I made in ninth grade. It's been pretty fun to do; there are a lot of times when its really dull and I get super fatigued, but there have been a few moments where I get something to work exactly how I want it to, and I think those make it worth it.",
+        " I've  been thinking about how much quicker it might have been if I just caved in and used like squarespace or something, but there is something about building it myself that really captures the entreprenuerial spirit of being stubborn, and I really relate to that."],
+    },
+   
+]
